41fc6d86601f387748b23c0d82610f2df12bad5e
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2024-01-11 11:28:59 -0400

    Some testing notes added

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,6 +15,29 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
+    // remember mocking an import
+    jest.mock('./app.service', () => ({
+      __esModule: true,
+      dependencyFunction: jest.fn(() => 'mockedValue'),
+    }));
+    // In case you want to mock something injectable you may need to create testing moudle and compile it
+    /*
+    // Create a testing module
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [SampleService, AnotherService],
+    }).compile();
+
+    // Get instances of the services
+    sampleService = module.get<SampleService>(SampleService);
+    anotherService = module.get<AnotherService>(AnotherService);
+  });
+
+  it('should return mocked data from AnotherService', () => {
+    // Mock the behavior of the getData method in AnotherService
+    const mockedData = 'mockedData';
+    (anotherService.getData as jest.Mock).mockReturnValue(mockedData);
+    // ...
+    */
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
